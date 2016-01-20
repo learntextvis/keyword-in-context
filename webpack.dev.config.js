25522c2cc5e527b9475ae0dc0e81a46885205dae
Yannick Assogba <yassogba@gmail.com>
2016-01-20 12:01:53 -0500

    fix incorrect property change in dev config.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,5 +2,5 @@ var config = require('./webpack.config.js');
 
 // In dev mode use the demo page as the main entry point.
 config.entry.main = './src/demo.jsx';
-config.entry.debug = true;
+config.debug = true;
 module.exports = config;
